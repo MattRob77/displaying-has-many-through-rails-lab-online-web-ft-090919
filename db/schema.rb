9e384cdc291bfcbf3e3e21ab8c3ab4db7f4a0a0b
Matthew Robinson <matt_robinson1@yahoo.com>
2019-11-13 18:04:54 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,26 @@
 ActiveRecord::Schema.define(version: 20191113175619) do
 
   create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_datetime"
+    t.string   "patient"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.integer  "doctor_id"
+    t.integer  "patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "department"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "patients", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
